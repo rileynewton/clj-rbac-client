1952049875e47b9aa9443340ff37985723b8b147
Jenkins CI <ci@puppetlabs.com>
2016-02-03 13:21:16 -0800

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
    :password :env/nexus_jenkins_password
    :sign-releases false})
 
-(defproject puppetlabs/pe-clients "0.1.0-SNAPSHOT"
+(defproject puppetlabs/pe-clients "0.1.0"
   :description "Clients for pe services"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
