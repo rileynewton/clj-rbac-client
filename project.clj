4af09323cbf10074f4fd1975d02145019120bc90
Michael Smith <michael.smith@puppet.com>
2017-03-16 15:02:10 -0700

    (maint) Bump clj-parent to 0.4.1

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
 
-  :parent-project {:coords [puppetlabs/clj-parent "0.3.2"]
+  :parent-project {:coords [puppetlabs/clj-parent "0.4.1"]
                    :inherit [:managed-dependencies]}
 
   :dependencies [[org.clojure/clojure]
