556a4d5a52bbc74ba4d67da015a3d0d22fde5842
jonathannewman <jonathan.newman@puppetlabs.com>
2017-04-05 10:36:52 -0700

    (maint) change version to 0.7.0 series
    This corresponds to the 2017.3.x PE series

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
    :password :env/nexus_jenkins_password
    :sign-releases false})
 
-(defproject puppetlabs/rbac-client "0.6.3-SNAPSHOT"
+(defproject puppetlabs/rbac-client "0.7.0-SNAPSHOT"
   :description "Tools for interacting with PE RBAC"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
