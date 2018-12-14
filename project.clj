9bcf7501a455c7f33b4f00a15b8ba4918b397981
Jenkins CI <ci@puppet.com>
2018-12-14 00:46:46 +0000

    Version 0.9.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
    :password :env/clojars_jenkins_password
    :sign-releases false})
 
-(defproject puppetlabs/rbac-client "0.9.4"
+(defproject puppetlabs/rbac-client "0.9.5-SNAPSHOT"
   :description "Tools for interacting with PE RBAC"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
